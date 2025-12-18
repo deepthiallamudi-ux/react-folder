7308df5cc8e35ef1cfedf0a4aca4d14059ad7e6b
deepthiallamudi-ux <deepthiallamudi@gmail.com>
2025-12-18 21:16:32 +0530

    new

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,11 +1,11 @@
 
 import React from 'react';  
-import {Link} from 'react-router-dom';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Home from './routing/Home.jsx';
-import About from './routing/About.jsx';
-import Todos from './routing/Todos.jsx';
-import NotFound from './routing/notFound.jsx';
+// import {Link} from 'react-router-dom';
+// import {Routes, Route, BrowserRouter} from 'react-router-dom';
+// import Home from './routing/Home.jsx';
+// import About from './routing/About.jsx';
+// import Todos from './routing/Todos.jsx';
+// import NotFound from './routing/notFound.jsx';
 // import Toggle from './toggle.jsx';
 // import Colorchange from './colorchange.jsx';
 //  import IterateFetch from './iteration&Fetch.jsx';
@@ -14,8 +14,7 @@ import NotFound from './routing/notFound.jsx';
 //   return(
 //     <div>
 //       <IterateFetch/>
-//       <Toggle/>
-//       <Colorchange/>
+
 //     </div>
 //   )
 // }
@@ -23,16 +22,19 @@ import NotFound from './routing/notFound.jsx';
 function App() {
   return(
     <>
-    {/* <BrowserRouter>
+    {/* <Toggle/>
+        <Colorchange/> 
+    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/todos' element={<Todos/>}/>
       <Route path='*' element={<NotFound/>}/>
     </Routes>
-    </BrowserRouter> */}
-    {/* <IterateFetch/> */}
+    </BrowserRouter>
+     <IterateFetch/> */}
 <UserData/>
+
     </>
   ) 
 }
