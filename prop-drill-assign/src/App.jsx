b46dc3ec6e75f15fd5dc905acf0b7802803bc008
deepthiallamudi-ux <deepthiallamudi@gmail.com>
2025-12-22 22:22:20 +0530

    push

--- prop-drill-assign/src/App.jsx
+++ prop-drill-assign/src/App.jsx
@@ -1,9 +1,11 @@
 import Todos from './todos/Todos.jsx';
+import Component1 from './prop-drill/component1.jsx';
 
 function App() {
   return (
     <div>
       <Todos />
+      <Component1 />
     </div>
   );
 }
